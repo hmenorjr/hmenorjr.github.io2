27cda70607e301cac36c1ae91bb9fad5d776467a
Herman Menor, Jr. <herman.menor@gmail.com>
2025-07-18 10:55:51 +0000

    Fix the TITLE and SITE_TITLE in the header.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "So Basic";
-export const SITE_DESCRIPTION = "Welcome to So Basic! Your new Astro template";
+export const SITE_TITLE = "HMenorJr";
+export const SITE_DESCRIPTION = "Welcome to HMenorjr! Blog site of Herman Menor Jr, writing about technology and gadgets use for his daily life.";
